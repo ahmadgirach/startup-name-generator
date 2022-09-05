19699fb81a4cc641338a08be3475397681025591
Ahmad Girach <ahmedkhatri6@gmail.com>
2022-09-05 10:49:08 +0530

    [INIT] structure of React, TypeScript & Tailwind

---
+++ README.md
@@ -0,0 +1,3 @@
+# Startup name generator
+
+- Web application to generate startup names. Built with React, TypeScript and Tailwind CSS.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center">
+      <h3 className='text-3xl font-bold'>Hola!</h3>
+    </div>
+  );
+}
+
+export default App;
